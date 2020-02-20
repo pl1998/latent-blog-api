5ba7fa08f499cfcbe3f1d76730f570c994a03437
pl1998 <2139714249@qq.com>
2020-02-20 16:39:57 +0800

    重写showLoginForm 方法

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -3,8 +3,17 @@
 namespace App\Admin\Controllers;
 
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
+use Illuminate\Http\Request;
 
 class AuthController extends BaseAuthController
 {
-
+    public function showLoginForm(Request $request)
+    {
+        if ($request->session()->has('url.intended')) {
+            if (strpos($request->session()->get('url.intended'), '/admin')) {
+                $request->session()->forget('url.intended');
+            }
+        }
+        return view('auth.login');
+    }
 }
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,15 +38,6 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function showLoginForm(Request $request)
-    {
-        if ($request->session()->has('url.intended')) {
-            if (strpos($request->session()->get('url.intended'), '/admin')) {
-                $request->session()->forget('url.intended');
-            }
-        }
-        return view('auth.login');
-    }
 
 
 }
